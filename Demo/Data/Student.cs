80d0e5490a0796bfc77a8d0667c3315562dbc9b1
Ali Besharati <1368.ali.b@gmail.com>
2020-04-24 11:18:06 +0430

    Add project files.

---
+++ Demo/Data/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Demo.Data
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public DateTime BeginDate { get; set; }
+
+       
+    }
+}
---
+++ InputDateIos/Extentions/PersianDateTimeBindConvertor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Microsoft.AspNetCore.Components
+{
+    public class PersianDateTimeBindConvertor
+    {
+        public static bool TryPresianConvertToDateTime(string Date, out DateTime value, out string ErrorMessage)
+        {
+            try
+            {
+                var PersianDate = Date.Split("/");
+                PersianCalendar pc = new PersianCalendar();
+                CultureInfo.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+                DateTime dt = new DateTime(int.Parse(PersianDate[0]), int.Parse(PersianDate[1]), int.Parse(PersianDate[2]), pc);
+                value = dt;
+                ErrorMessage = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                value = default;
+                return false; ;
+            }
+
+        }
+
+    }
+}
